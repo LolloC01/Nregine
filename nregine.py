fe1c02a6df2f37785f61620b895f535255c4aee9
LolloC01 <colittolorenzo@gmail.com>
2024-06-04 11:23:35 +0200

    v

--- nregine.py
+++ nregine.py
@@ -1,36 +1,96 @@
-import numpy as np
+from random import randint
+from provamatrice import get_diagonals
 
-#chess = [[0 for y in range(8)] for x in range(8)]
-
-chess = []
-for x in range(8):
-    c = []
-    for y in range(8):
-        c.append(y)
-    chess.append(c)
+chess = [[0 for y in range(8)] for x in range(8)]
 
 s: list = []
 
+
+no_queen = {
+    0 : [0, 1, 2, 3, 4, 5, 6, 7],
+    1 : [0, 1, 2, 3, 4, 5, 6, 7],
+    2 : [0, 1, 2, 3, 4, 5, 6, 7],
+    3 : [0, 1, 2, 3, 4, 5, 6, 7],
+    4 : [0, 1, 2, 3, 4, 5, 6, 7],
+    5 : [0, 1, 2, 3, 4, 5, 6, 7],
+    6 : [0, 1, 2, 3, 4, 5, 6, 7],
+    7 : [0, 1, 2, 3, 4, 5, 6, 7]
+}
+
+def restart() -> dict:
+    queen = {
+        0 : [0, 1, 2, 3, 4, 5, 6, 7],
+        1 : [0, 1, 2, 3, 4, 5, 6, 7],
+        2 : [0, 1, 2, 3, 4, 5, 6, 7],
+        3 : [0, 1, 2, 3, 4, 5, 6, 7],
+        4 : [0, 1, 2, 3, 4, 5, 6, 7],
+        5 : [0, 1, 2, 3, 4, 5, 6, 7],
+        6 : [0, 1, 2, 3, 4, 5, 6, 7],
+        7 : [0, 1, 2, 3, 4, 5, 6, 7]
+    }
+    return queen
+
+def print_chess() -> None:
+    for x in chess:
+        print(x)
+
 def check(pos: tuple) -> bool:
-    """for raw in range(8):
+    for raw in range(8):
         if chess[raw][pos[1]] == 1:
             return False
     for raw in range(8):
         if chess[pos[0]][raw] == 1:
-            return False"""
-    diag1 = np.diagonal(chess,pos[0])
-    print(diag1)
-    diag2 = np.fliplr(chess)
-    diag_2 = np.diagonal(diag2,-pos[1])
-    print(diag_2)
+            return False
+    diag1, diag2 = get_diagonals(chess, pos)
+    if 1 in diag1:
+        return False
+    if 1 in diag2:
+        return False
     return True
-        
 
-def print_m(chess: list) -> None:
-    for x in chess:
-        print(x)
+def chek_pos(x: int) -> list:
+    """crea la lista con le posizioni in cui è possibile mettere una regina
 
-chess[2][4] = 1
-print_m(chess)
-print(chess[2][4])
-print(check((2,4)))
+    Args:
+        x (int): indica la riga da controllare
+
+    Returns:
+        ris (list): lista di tuple contenente le posizioni possibili
+    """
+    ris = []
+    for y in range(8):
+        if check(x,y):
+            ris.append((x,y))
+    return ris
+
+def prima_riga() -> None:
+    y = randint(0,7)
+    s.append((0,y))
+    chess[0][y] = 1
+
+def check_last_row() -> bool:
+    pos = chek_pos()
+    if len(pos) != 0:
+        return True
+    return False
+
+def righe() -> None:
+    pass
+
+def n_queens(row: int, avanti: bool = True) -> bool:
+    if avanti:
+        if row == 0:
+            prima_riga()
+        elif row == 7:    ###Check ultima riga: se ci sono posizioni in cui mettere la regina ho finito altrimenti torno indietro
+            pos = chek_pos(7)
+            if len(pos) != 0:
+                chess[7][pos[0]]
+                s.append((7,pos(0)))
+                return s
+            else:
+                pass ###ROLLBACK
+        else:
+            pos = chek_pos(row)
+            if len(pos) != 0:
+    else:
+        pass
--- provamatrice.py
+++ provamatrice.py
@@ -1,10 +1,41 @@
-import numpy as np
+def get_diagonals(matrix: list[list[int]], pos: tuple) -> tuple[list[int]]:
+    """
+    ritorna la diagonale principale e l'antidiagonale della matrice data la posizione pos
 
-chess = []
-for x in range(8):
-    c = []
-    for y in range(8):
-        c.append(y)
-    chess.append(c)
-for x in chess:
-    print(x)
+    Args:
+        matrix (list[list[int]]): matrice in cui cercare la posizione
+        pos (tuple): posizione da cercare
+
+    Returns:
+        tuple[list[int]]: lista di tuple contenenti diagonale e anti-diagonale
+    """
+    row: int = pos[0]
+    col: int = pos[1]
+
+    diagonal: list[int] = []
+    anti_diagonal: list[int] = []
+
+    matrix_size: int = len(matrix)
+
+    # Calcolo della diagonale
+    if row < col:
+        for r, c in zip(range(0, matrix_size), range(col - row, matrix_size)):
+            diagonal.append(matrix[r][c])
+    else:
+        for r, c in zip(range(row - col, matrix_size), range(0, matrix_size)):
+            diagonal.append(matrix[r][c])
+
+    # Calcolo della anti-diagonale
+    if row + col < 7:
+        start_row = 0
+        start_col = row + col
+    else:
+        start_row = row + col - 7
+        start_col = 7
+
+    while start_row < matrix_size and start_col >= 0:
+        anti_diagonal.append(matrix[start_row][start_col])
+        start_row += 1
+        start_col -= 1
+
+    return diagonal, anti_diagonal
